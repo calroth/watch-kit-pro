a13a2dc7e744027b7f2776044dbfea8743ca7a50
Terence Tan <~@calroth.net>
2019-05-19 21:03:54 +1000

    SettingsConfigData: initial revision
    Move code from ComplicationConfigData.

--- Wearable/src/main/java/pro/watchkit/wearable/watchface/model/ComplicationConfigData.java
+++ Wearable/src/main/java/pro/watchkit/wearable/watchface/model/ComplicationConfigData.java
@@ -13,23 +13,6 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * This file incorporates work covered by the following copyright and
- * permission notice:
- *
- *     Copyright (C) 2017 The Android Open Source Project
- *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
- *
- *          http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
  */
 
 package pro.watchkit.wearable.watchface.model;
@@ -46,19 +29,7 @@ public class ComplicationConfigData extends ConfigData {
     public List<ConfigItemType> getDataToPopulateAdapter(Context context) {
         return Arrays.asList(
                 // Complication picker from watch face drawable.
-                new WatchFaceDrawableConfigItem(R.drawable.add_complication),
-
-                // Data for 'Unread Notifications' UX (toggle) in settings Activity.
-                new UnreadNotificationConfigItem(
-                        context.getString(R.string.config_unread_notifications_label),
-                        R.drawable.ic_notifications_white_24dp,
-                        R.drawable.ic_notifications_off_white_24dp),
-
-                // Data for 'Night Vision' UX (toggle) in settings Activity.
-                new NightVisionConfigItem(
-                        context.getString(R.string.config_night_vision_label),
-                        R.drawable.ic_notifications_white_24dp,
-                        R.drawable.ic_notifications_off_white_24dp)
+                new WatchFaceDrawableConfigItem(R.drawable.add_complication)
         );
     }
 }
